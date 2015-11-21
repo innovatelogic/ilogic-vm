be2b0931ec50c9e47245dd19503e5a3ee637186b
YurG <yura.gunko@gmail.com>
2015-11-22 00:07:36 +0200

    upd

--- Build/clear_all.cmd
+++ Build/clear_all.cmd
@@ -1,4 +1,5 @@
-call config.cmd
+@echo off
+call %~dp0\config.cmd
 
-rmdir /Q /S %~dp0..\..\%OUT_FOLDER_32%
-rmdir /Q /S %~dp0..\..\%OUT_FOLDER_64%
+if exist %~dp0..\..\%OUT_FOLDER_32%\ ( rmdir /Q /S %~dp0..\..\%OUT_FOLDER_32% && echo clear32 ok) ELSE (echo clear32 not exist)
+if exist %~dp0..\..\%OUT_FOLDER_64%\ ( rmdir /Q /S %~dp0..\..\%OUT_FOLDER_64% && echo clear64 ok) ELSE (echo clear64 not exist)
