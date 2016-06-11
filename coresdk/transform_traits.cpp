8947e266dd455e88dfbb8c8752442539119515c1
Yura Gunko <Yura Gunko>
2016-06-11 22:37:09 +0300

    add missing file

---
+++ coresdk/transform_traits.cpp
@@ -0,0 +1,16 @@
+#include "transform_traits.h"
+//#include "IDrawInterface.h"
+
+namespace core_sdk_api
+{
+    void transform_traits::Foo(const Matrix &view)
+    {
+
+    }
+
+    //----------------------------------------------------------------------------------------------
+    //void transform_traits::TransformPosition(IDrawInterface *object, const Vector &pivot, const Vector &ypr)
+    //{
+
+    //}
+}
