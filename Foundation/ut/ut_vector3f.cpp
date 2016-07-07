52bb9d03b7c241780baf1ef31508bd03eb29edac
YurG <yura.gunko@gmail.com>
2016-07-07 15:33:20 +0300

    vector ut

--- Foundation/ut/ut_vector3f.cpp
+++ Foundation/ut/ut_vector3f.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include <gtest/gtest.h>
 
-/*#include "../vector.h"
+#include "vector.h"
 
 //----------------------------------------------------------------------------------------------
 TEST(TestMath, AddTwoVectors)
 {
-	Vector one(1.5f, 2.5f, 3.5f);
-	Vector two(1.f, 1.f, 1.f);
+    oes::common_base::Vector one(1.5f, 2.5f, 3.5f);
+    oes::common_base::Vector two(1.f, 1.f, 1.f);
 
-	Vector res = one + two;
+    oes::common_base::Vector res = one + two;
 
 	EXPECT_EQ(res.x, 2.5f);
 	EXPECT_EQ(res.y, 3.5f);
@@ -19,12 +19,12 @@ TEST(TestMath, AddTwoVectors)
 //----------------------------------------------------------------------------------------------
 TEST(TestMath, SubstractTwoVectors)
 {
-	Vector one(1.5f, 2.5f, 3.5f);
-	Vector two(1.f, 1.f, 1.f);
+    oes::common_base::Vector one(1.5f, 2.5f, 3.5f);
+    oes::common_base::Vector two(1.f, 1.f, 1.f);
 
-	Vector res = one - two;
+    oes::common_base::Vector res = one - two;
 
 	EXPECT_EQ(res.x, 0.5f);
 	EXPECT_EQ(res.y, 1.5f);
 	EXPECT_EQ(res.z, 2.5f);
-}*/
+}
