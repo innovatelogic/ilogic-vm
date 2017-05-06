6b46f10dfe36b701de49ee2d5bfb252e499fda56
yurg <westgames@gmail.com>
2017-05-06 22:24:41 +0300

    matrix ut transform

--- BaseCommon/ut/ut_matrix_transform.cpp
+++ BaseCommon/ut/ut_matrix_transform.cpp
@@ -22,19 +22,37 @@ namespace oes
 
         TEST(Math, Vector_matrix_chain_transform)
         {
+            //                          transform
             //      root
-            //       |
-            //     child0
-            //       |
-            //     child2
+            //       |                          Y        
+            //     child0                       |      Z
+            //       |                          |    /
+            //     child1                       |   /
+            //       |                          |  /
+            //     child2                        ____________ X
+            //       |    
+            //    [loc_pos]
+            //
+            //      
 
             Matrix root;
             Matrix child0;
             Matrix child1;
+            Matrix child2;
 
             root.Identitly();
-            child0.SetRotateAxis(Vector(1.f, 0.f, 0.f), m_pi);
+            child0.SetRotateAxis(Vector(1.f, 0.f, 0.f), m_pi * 0.5f); // X->X; Y->-Z; Z->Y 
+            child1.SetRotateAxis(Vector(0.f, 1.f, 0.f), m_pi * 0.5f);
+            child2.SetRotateAxis(Vector(0.f, 0.f, 1.f), m_pi * 0.5f);
 
+            Matrix world = root * child0 * child1 * child2;
+
+            Vector loc_pos(1.f, 0.f, 0.f);
+
+            Vector vworld;
+            transform_coord(vworld, loc_pos, world);
+
+            //EXPECT_EQ(world.row0, vlocal);
         }
     }
 }
