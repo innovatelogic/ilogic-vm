e8c82134e062ea297bbda10450f21dd40d637187
YurG <yura.gunko@gmail.com>
2017-07-06 00:11:02 +0300

    file_cache.py

---
+++ Build/scripts/file_cache.py
@@ -0,0 +1,19 @@
+import os
+
+class FileCache:
+    def __init__(self, root, file_cache):
+        self.root = root
+        self.file_cache = file_cache
+        pass
+
+    def rebuild_all(self):
+        pass
+
+    def check(self):
+        pass
+
+    def load(self):
+        pass
+
+    def save(self):
+        pass
